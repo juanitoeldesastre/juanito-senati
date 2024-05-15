be52d72744ac9973db0e460a0b21fcd75395b446
juanitoeldesastre <juanitoeldevastador@gmail.com>
2024-05-15 16:59:59 -0500

    first commit

---
+++ php-examples/02-variables.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombres = "juan piero";
+$apellidos = "vincha loza";
+$curso = "BACKEND DEVELOPER WEB";
+$semestre = "3";
+
+
+echo $nombres . " " . $apellidos . " " . $curso . " " . $semestre;
+
+?>
---
+++ php-examples/03-operaciones.php
@@ -0,0 +1,16 @@
+<?php
+
+$numero1 = 10;
+$numero2 = 20;
+
+$suma = $numero1 + $numero2;
+$resta = $numero1 - $numero2;
+$multiplicacion = $numero1 * $numero2;
+$division = $numero1 / $numero2;
+
+echo "La suma es " . $suma . "<br>";
+echo "La resta es " . $resta . "<br>";
+echo "La multiplicación es " . $multiplicacion . "<br>";
+echo "La división es " . $division . "<br>";
+
+?>
