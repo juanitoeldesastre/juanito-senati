2b412c43cb160341138bf529e6fad0feafcfa292
juanitoeldesastre <juanitoeldevastador@gmail.com>
2024-05-21 17:09:09 -0500

    eyou ai laik ur as darki boys

--- php-examples/12-clase.php
+++ php-examples/12-clase.php
@@ -1,26 +1,46 @@
-class calculadora {
+<?php
 
-public $primerNumero;
-public $segundoNumero;
+class Calculadora {
+    public $primerNumero;
+    public $segundoNumero;
 
-public Calculadora($num1 = 0, $num2 = 0) {
-    $this->primerNumero=$num1;
-    $this->segundoNumero=$num2;
+    public function __construct($num1 = 0, $num2 = 0) {
+        $this->primerNumero = $num1;
+        $this->segundoNumero = $num2;
+    }
+
+    public function sumar() {
+        return $this->primerNumero + $this->segundoNumero;
+    }
+
+    public function restar() {
+        return $this->primerNumero - $this->segundoNumero;
+    }
+
+    public function multiplicar() {
+        return $this->primerNumero * $this->segundoNumero;
+    }
+
+    public function dividir() {
+        // Verifica si el divisor es 0 para evitar una división por cero
+        if ($this->segundoNumero != 0) {
+            return $this->primerNumero / $this->segundoNumero;
+        } else {
+            return "Error: División por cero";
+        }
+    }
+
+    public function potencia() {
+        return pow($this->primerNumero, $this->segundoNumero);
+    }
 }
 
-public function sumar() {
-    return $primerNumero+$segundoNumero;
-}
+// Ejemplo de uso
+$miCalculadora = new Calculadora(5, 2);
+echo "Suma: " . $miCalculadora->sumar() . "<br>";
+echo "Resta: " . $miCalculadora->restar() . "<br>";
+echo "Multiplicación: " . $miCalculadora->multiplicar() . "<br>";
+echo "División: " . $miCalculadora->dividir() . "<br>";
+echo "Potencia: " . $miCalculadora->potencia() . "<br>";
 
-public function restar() {
-    return $primerNumero-$segundoNumero;
-}
-
-public function multiplicar() {
-    return $primerNumero*$segundoNumero;
-}
-
-public function dividir() {
- 
-   return $primerNumero/$segundoNumero;
-}
+?>
