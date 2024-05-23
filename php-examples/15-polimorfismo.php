3c6dc646b9fad090057e0c042c0bae3c513cf026
juanitoeldesastre <juanitoeldevastador@gmail.com>
2024-05-23 16:55:21 -0500

    sigma mewing

--- php-examples/15-polimorfismo.php
+++ php-examples/15-polimorfismo.php
@@ -59,3 +59,5 @@ class Circulo implements Figura {
         return $area;
     }
 }
+
+?>
--- php-examples/15.1-programa.php
+++ php-examples/15.1-programa.php
@@ -1,22 +1,37 @@
 <?php
 
-include("15-polimorfismo.php")
+include("15-polimorfismo.php");
 
 echo "AREA DE TRIANGULO\n";
 echo "INGRESE LA BASE :";
-$b=fgets(STDIN);
+$b = (float)trim(fgets(STDIN));
 echo "INGRESE LA ALTURA :";
-$h=fgets(STDIN);
-$triangulo = new Triangulo(4,8);
-$area=$triangulo->calcularArea();
-echo "EL AREA DEL TRIANGULO ES ".$area;
+$h = (float)trim(fgets(STDIN));
+$triangulo = new Triangulo($b, $h);
+$area = $triangulo->calcularArea();
+echo "EL AREA DEL TRIANGULO ES " . $area . "\n";
 
 echo "\nAREA DE RECTANGULO\n";
 echo "INGRESE LA BASE :";
-$b=fgets(STDIN);
+$b = (float)trim(fgets(STDIN));
 echo "INGRESE LA ALTURA :";
-$a=fgets(STDIN);
-$rectangulo = new Rectangulo($b,$a);
-$area=$rectangulo->calcularArea();
-echo"EL AREA DEL RECTANGULO ES".$area;
+$a = (float)trim(fgets(STDIN));
+$rectangulo = new Rectangulo($b, $a);
+$area = $rectangulo->calcularArea();
+echo "EL AREA DEL RECTANGULO ES " . $area . "\n";
+
+echo "\nAREA DE CUADRADO\n";
+echo "INGRESE EL LADO :";
+$l = (float)trim(fgets(STDIN));
+$cuadrado = new Cuadrado($l);
+$area = $cuadrado->calcularArea();
+echo "EL AREA DEL CUADRADO ES " . $area . "\n";
+
+echo "\nAREA DE CIRCULO\n";
+echo "INGRESE EL RADIO :";
+$r = (float)trim(fgets(STDIN));
+$circulo = new Circulo($r);
+$area = $circulo->calcularArea();
+echo "EL AREA DEL CIRCULO ES " . $area . "\n";
+
 ?>
