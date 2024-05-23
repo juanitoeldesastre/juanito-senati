bc5da4d822e05d2d9eb73cde8f229b2c6c46899f
juanitoeldesastre <juanitoeldevastador@gmail.com>
2024-05-23 16:53:53 -0500

    sigma mewing

--- php-examples/15-polimorfismo.php
+++ php-examples/15-polimorfismo.php
@@ -1,38 +1,61 @@
 <?php
 
-  interface Figura {
+interface Figura {
     public function calcularArea();
-  }
+}
 
-  class Triangulo implements Figura {
+class Triangulo implements Figura {
     public $base;
     public $altura;
 
-    public function __construct($b,$h) {
-      $this->base = $b;
-      $this->altura = $h;
+    public function __construct($b, $h) {
+        $this->base = $b;
+        $this->altura = $h;
     }
 
     public function calcularArea() {
-      $area = ($this->base * $this->altura) / 2;
-      return $area;
-
+        $area = ($this->base * $this->altura) / 2;
+        return $area;
     }
-  }
+}
 
-  class Rectangulo implements Figura {
+class Rectangulo implements Figura {
     public $base;
     public $altura;
 
-    public function __construct($b,$a) {
-      $this->base = $b;
-      $this->altura = $a;
+    public function __construct($b, $a) {
+        $this->base = $b;
+        $this->altura = $a;
     }
 
     public function calcularArea() {
-      $area = ($this->base * $this->altura);
-      return $area;
+        $area = ($this->base * $this->altura);
+        return $area;
     }
-  }
+}
 
-?>
+class Cuadrado implements Figura {
+    public $lado;
+
+    public function __construct($l) {
+        $this->lado = $l;
+    }
+
+    public function calcularArea() {
+        $area = $this->lado * $this->lado;
+        return $area;
+    }
+}
+
+class Circulo implements Figura {
+    public $radio;
+
+    public function __construct($r) {
+        $this->radio = $r;
+    }
+
+    public function calcularArea() {
+        $area = M_PI * $this->radio * $this->radio;
+        return $area;
+    }
+}
