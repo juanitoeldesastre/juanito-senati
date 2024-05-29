175b46a81cf65fe303130a4f4f686cc56bf7f2fd
juanitoeldesastre <juanitoeldevastador@gmail.com>
2024-05-29 17:44:09 -0500

    sigma mewing

--- php-examples/18-pdo-select.php
+++ php-examples/18-pdo-select.php
@@ -1,17 +1,18 @@
 <?php
 
 try {
-    $dsn="mysql:host=localhost;dbname=covid";//dsn: data source name , nombre origen de datos
-    $user="root";//user : usuario
-    $pass="";//pass : clave de usuario
+    $dsn = "mysql:host=localhost;dbname=covid";
+    $user = "root";
+    $pass = "root";
     $db = new PDO($dsn, $user, $pass);
-    echo "Hola base de datos tengo conexion\n";
-    $pacientes = $db->query('SELECT * FROM pacientes where 14<=edad AND edad<=19');//edad BETWEEN 18 AND 19
+    echo "Hola base de datos, tengo conexión\n";
+
+    $pacientes = $db->query('SELECT * FROM pacientes WHERE edad BETWEEN 14 AND 19 ORDER BY edad ASC');
+
     foreach ($pacientes as $row) {
-        echo $row["nombres"]." ".$row["apellidos"].", EDAD : ".$row["edad"]."\n";
-    }    
+        echo $row["nombres"] . " " . $row["apellidos"] . ", EDAD: " . $row["edad"] . "\n";
+    }
 } catch (PDOException $e) {
-    // attempt to retry the connection after some timeout for example
-    echo "Error : ".$e->getMessage();
+    echo "Error: " . $e->getMessage();
 }
 ?>
