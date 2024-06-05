cb56a1443f28f63d208cc582652655cdc7fc3791
juanitoeldesastre <juanitoeldevastador@gmail.com>
2024-06-05 16:13:40 -0500

    sigma mewing

--- php-examples/25.1-ajax.js
+++ php-examples/25.1-ajax.js
@@ -11,10 +11,29 @@ function buscarPacientes() {
         data: datos,
         success : function(result) {
             console.log(result);
-
+            debugger;
+            //console.ing(result);
+            const pacientes = result;
+            paciente.forEach(item => {
+                agregarFilas("#tabla",item);
+            });           
         }     
     })
     
-    
-    return;
+    return; 
+}
+
+function agregarFilas(id) {
+    const html =
+    "<tr>"+
+    "<td>"+Paciente.Nombres+"</td>"+
+    "<td>"+Paciente.Edad+"</td>"+
+    "<td>"+Paciente.Talla+"</td>"+
+    "<td>"+Paciente.Peso+"</td>"+
+    "<td>"+Paciente.Tos+"</td>"+
+    "<td>"+Paciente.Fiebre+"</td>"+
+    "<td>"+Paciente.Disnea+"</td>"+
+    "<td><button type='button'>Editar</button></td>" +
+    "</tr>";
+    $(id + " tr:last").after(html);
 }
