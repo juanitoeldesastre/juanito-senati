959a363247c4b3bb262678829b19d1d3a16080b2
juanitoeldesastre <juantoeldevastador@gmail.com>
2024-06-24 14:38:16 -0500

    hola mundo

--- php-examples/01-holamundo.php
+++ php-examples/01-holamundo.php
@@ -1,3 +1,19 @@
-<?php echo 'Hola Mundo.'; ?> 
+<title>Hola Mundo</title>
+    <style> 
+        :root {
+            color-scheme: light dark;
+        }
 
+        body {
+            display: grid;
+            place-content: center;
+            font-family: Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <?php echo "Hola Mundo"; ?> 
+    </h1>
+</body>
 
