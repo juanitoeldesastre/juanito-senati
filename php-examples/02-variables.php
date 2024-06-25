58ccc6ed98a276d243090a46d466d7c9a12520f2
juanitoeldesastre <juantoeldevastador@gmail.com>
2024-06-25 17:57:34 -0500

    Variables and icon

--- php-examples/02-variables.php
+++ php-examples/02-variables.php
@@ -1,10 +1,35 @@
-<?php
-
-$nombres ="JUAN PIERO";
-$apellidos="VINCHA LOZA";
-$curso="BACKEND DEVELOPER WEB";
-$semestre="TERCER SEMESTRE";
-//juntar nombres apellidos curso y semestre
-echo $nombres." ".$apellidos." ".$curso." ".$semestre;
-
-?>
+<link rel="icon" href="php.ico"> 
+<title>Variables</title>
+    <style>
+        :root {
+            color-scheme: light dark;
+        }
+        body {
+            display: grid;
+            place-content: center;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 100px;
+        }
+        h1 {
+            font-size: 1.5em;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $nombres = "Juan Piero";
+    $apellidos = "Vincha Loza";
+    $edad = 18; 
+    $curso = "Backend Developer Web";
+    $semestre = "tercer semestre";
+    
+    $nuevaedad = $edad + 1;
+    
+    // Crear mensaje
+    $mensaje = "Hola, $nombres $apellidos. Tienes $nuevaedad años, estás en el curso de $curso y vas en el $semestre.";
+    ?>
+    <h1>
+        <?= $mensaje; ?>
+    </h1>
+</body>
